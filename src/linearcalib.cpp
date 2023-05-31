a94f82069eca41a7d8a26f2612d1e1a06d89bf62
Stefan Trippler <stefan.trippler@betterask.erni>
2023-05-31 16:29:03 +0200

    Initial check-in

---
+++ src/linearcalib.cpp
@@ -0,0 +1,56 @@
+
+#include "linearcalib.h"
+
+
+
+LinearCalib::LinearCalib(const float f32_m, const float f32_n)
+  : mf32_m{f32_m}
+  , mf32_n{f32_n}
+{
+}
+
+
+
+void LinearCalib::setParams(const float f32_m, const float f32_n)
+{
+  mf32_m=f32_m;
+  mf32_n=f32_n;
+}
+
+
+
+int LinearCalib::setParams(const float f32_set1, const float f32_mess1, const float f32_set2, const float f32_mess2)
+{
+  if(0==(f32_mess2-f32_mess1))
+    return -1; // division by zero - do nothing
+
+  mf32_m=(f32_set2-f32_set1)/(f32_mess2-f32_mess1);
+  mf32_n=f32_set1-mf32_m*f32_mess1;
+
+  return 0;
+}
+
+
+
+void LinearCalib::getParams(float *pf32_m, float *pf32_n)
+{
+  if(pf32_m)
+    *pf32_m = mf32_m;
+  if(pf32_n)
+    *pf32_n = mf32_n;
+}
+
+
+
+float LinearCalib::calc(const float f32_Value)
+{
+  return mf32_m*f32_Value+mf32_n;
+}
+
+
+
+float LinearCalib::map(const float f32_Value, const float f32_InMin, const float f32_InMax, const float f32_OutMin, const float f32_OutMax)
+{
+  return (f32_Value-f32_InMin)*(f32_OutMax-f32_OutMin)/(f32_InMax-f32_InMin)+f32_OutMin;
+}
+
---
+++ src/linearcalib.h
@@ -0,0 +1,26 @@
+
+#ifndef _LIN_CALIB_H_
+#define _LIN_CALIB_H_
+
+#include <inttypes.h>
+
+
+class LinearCalib
+{
+  float mf32_m;
+  float mf32_n;
+
+public:
+  LinearCalib(const float f32_m = 1.0, const float f32_n = 0.0);
+
+  void setParams(const float f32_m, const float f32_n);
+  int setParams(const float f32_set1, const float f32_mess1, const float f32_set2, const float f32_mess2);
+  void getParams(float *pf32_m, float *pf32_n);
+
+  float calc(const float f32_Value);
+
+  static float map(const float f32_Value, const float f32_InMin, const float f32_InMax, const float f32_OutMin, const float f32_OutMax);
+};
+
+
+#endif // _LIN_CALIB_H_
