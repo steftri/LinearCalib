57ba1066fd197fddaa9731ab95251371535a7076
Stefan Trippler <stefan.trippler@betterask.erni>
2023-06-14 13:07:29 +0200

    safety check for division by zero

--- src/linearcalib.cpp
+++ src/linearcalib.cpp
@@ -55,6 +55,8 @@ float LinearCalib::calc(const float f32_Value)
 
 float LinearCalib::map(const float f32_Value, const float f32_InMin, const float f32_InMax, const float f32_OutMin, const float f32_OutMax)
 {
+  if(0.0==(f32_InMax-f32_InMin))  // check for division by zero
+    return f32_Value;   // shit in, shit out
   return (f32_Value-f32_InMin)*(f32_OutMax-f32_OutMin)/(f32_InMax-f32_InMin)+f32_OutMin;
 }
 
